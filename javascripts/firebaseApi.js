6eaf121a9131c8159f259722a98d32d6e05b6078
Rita Patrick <rita.patrick1007@gmail.com>
2018-06-02 10:11:09 -0500

    delete and reprint works

--- javascripts/firebaseApi.js
+++ javascripts/firebaseApi.js
@@ -40,9 +40,24 @@ const getAllMovies = () => {
       });
   });
 };
+const deleteMovieFromDatabase = (movieId) => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      method: 'DELETE',
+      url: `${firebaseConfig.databaseURL}/movies/${movieId}.json`,
+    })
+      .done(() => {
+        resolve();
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};;
 
 module.exports = {
   saveMovietoWishList,
   setConfig,
   getAllMovies,
+  deleteMovieFromDatabase,
 };

ca2f9d4f97eb8f5e7121c814d119a7d74348caff
Rita Patrick <rita.patrick1007@gmail.com>
2018-06-04 21:23:43 -0500

    can register user

---
+++ javascripts/auth.js
@@ -0,0 +1,26 @@
+const {getAllMoviesEvent,} = require('./events');
+
+const checkLoginStatus = () => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      $('#myMovies').removeClass('hide');
+      $('#search').addClass('hide');
+      $('#authScreen').addClass('hide');
+      $('#mine, #navSearch, #logout').removeClass('hide');
+      $('#authenticate').addClass('hide');
+      // call the getMoviesEvent
+      getAllMoviesEvent();
+      // User is signed in.
+    } else {
+      // No user is signed in.
+      $('#myMovies').addClass('hide');
+      $('#search').addClass('hide');
+      $('#authScreen').removeClass('hide');
+      $('#mine, #navSearch, #logout').addClass('hide');
+      $('#authenticate').removeClass('hide');
+    }
+  });
+};
+module.exports = {
+  checkLoginStatus,
+};
